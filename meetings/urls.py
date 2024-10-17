b6ff0633fa1c14dbc8d7560ae14bd64236ed905d
hamza <hamzamekni4@gmail.com>
2024-10-17 10:42:05 +0100

    update meeting works

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import detail,meetings_list_view,add_meeting, delete_meeting
+from .views import detail,meetings_list_view,add_meeting, delete_meeting, update_meeting
 
 #domain.com/website/...
 urlpatterns=[
@@ -8,5 +8,6 @@ urlpatterns=[
     path('details/<int:id>', detail, name='detail'),
     path('new/', add_meeting, name='add'),
     path('delete/<int:id>/', delete_meeting, name='delete'),
+    path('update/<int:id>/', update_meeting, name='update_meeting'),
     path('new/', add_meeting, name='add'),
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -39,3 +39,15 @@ def delete_meeting(request, id):
         return redirect('meetings') # Redirect to the meetings list view after deletion
     return render(request, 'confirm_delete.html', {'meeting': meeting})
     # Render confirmation page
+
+# Vue pour mettre à jour une réunion existante
+def update_meeting(request, id):
+    meeting = get_object_or_404(Meeting, id=id) # Récupérer la réunion par ID
+    if request.method == "POST":
+        form = MeetingForm(request.POST, instance=meeting) # Passer l'instance à mettre à jour
+        if form.is_valid():
+            form.save() # Sauvegarder les modifications
+            return redirect('meetings') # Rediriger vers la liste des réunions
+    else:
+        form = MeetingForm(instance=meeting)  # Remplir le formulaire avec les données existantes
+    return render(request, 'update_meeting.html', {'form': form}) #Rendre le template
